6d4c9478c92c547428a4927860aa3b860c11c466
Cole Fiscus <coletfiscus@gmail.com>
2020-10-22 16:29:04 -0600

    Add ability to choose message and receive message. Small edits to styling and naming

--- main.js
+++ main.js
@@ -1,50 +1,90 @@
 var data = {
-  affirmations: {
-    1: 'I forgive myself and set myself free.',
-    2: 'I believe I can be all that I want to be.',
-    3: 'I am in the process of becoming the best version of myself.',
-    4: 'I have the freedom & power to create the life I desire.',
-    5: 'I choose to be kind to myself and love myself unconditionally.',
-    6: 'My possibilities are endless.',
-    7: 'I am worthy of my dreams.',
-    8: 'I am enough.',
-    9: 'I deserve to be healthy and feel good.',
-    10: 'I am full of energy and vitality and my mind is calm and peaceful.',
-    11: 'Every day I am getting healthier and stronger.',
-    12: 'I honor my body by trusting the signals that it sends me.',
-    13: 'I manifest perfect health by making smart choices.',
-  },
-  mantras: {
-    1: 'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
-    2: 'Don’t let yesterday take up too much of today.',
-    3: 'Every day is a second chance.',
-    4: 'Tell the truth and love everyone.',
-    5: 'I am free from sadness.',
-    6: 'I am enough.',
-    7: 'In the beginning it is you, in the middle it is you and in the end it is you.',
-    8: 'I love myself.',
-    9: 'I am present now.',
-    10: 'Inhale the future, exhale the past.',
-    11: 'This too shall pass.',
-    12: 'Yesterday is not today.',
-    13: 'The only constant is change.',
-    14: 'Onward and upward.',
-    15: 'I am the sky, the rest is weather.',
-  },
+  affirmations: [
+      'I forgive myself and set myself free.',
+      'I believe I can be all that I want to be.',
+      'I am in the process of becoming the best version of myself.',
+      'I have the freedom & power to create the life I desire.',
+      'I choose to be kind to myself and love myself unconditionally.',
+      'My possibilities are endless.',
+      'I am worthy of my dreams.',
+      'I am enough.',
+      'I deserve to be healthy and feel good.',
+      'I am full of energy and vitality and my mind is calm and peaceful.',
+      'Every day I am getting healthier and stronger.',
+      'I honor my body by trusting the signals that it sends me.',
+      'I manifest perfect health by making smart choices.',
+    ],
+
+  mantras: [
+      'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+      'Don’t let yesterday take up too much of today.',
+      'Every day is a second chance.',
+      'Tell the truth and love everyone.',
+      'I am free from sadness.',
+      'I am enough.',
+      'In the beginning it is you, in the middle it is you and in the end it is you.',
+      'I love myself.',
+      'I am present now.',
+      'Inhale the future, exhale the past.',
+      'This too shall pass.',
+      'Yesterday is not today.',
+      'The only constant is change.',
+      'Onward and upward.',
+      'I am the sky, the rest is weather.',
+    ],
 }
 
 // QUERY SELECTORS 👇👇👇👇👇👇
+var window = document.querySelector('.body')
 
 var affirmationSelection = document.querySelector('.affirmation-selection')
-var mantraSelection = document.querySelector(.'mantra-selection')
+var mantraSelection = document.querySelector('.mantra-selection')
 var receiveMessageButton = document.querySelector('.receive-message-button')
 
 var outputBox = document.querySelector('.output-box')
+var message = document.querySelector('.message')
 var yogaFigure = document.querySelector('.yoga-figure')
 
 // EVENT HANDLERS 👇👇👇👇👇👇
 
-affirmationSelection.addEventListener('click', )
-mantraSelection.addEventListener('click', )
+affirmationSelection.addEventListener('click', chooseAffirmation)
+mantraSelection.addEventListener('click', chooseMantra)
+receiveMessageButton.addEventListener('click', deliverMessage)
+
+window.addEventListener('click', deselectMessage)
 
 // FUNCTIONS 👇👇👇👇👇👇
+function returnRandomIndex(array) {
+  return array[(Math.floor(Math.random() * array.length))];
+}
+
+function chooseAffirmation() {
+  mantraSelection.checked = false
+}
+
+function chooseMantra() {
+  affirmationSelection.checked = false
+}
+
+function deliverMessage() {
+  yogaFigure.classList.add('hidden')
+  message.classList.remove('hidden')
+  if (affirmationSelection.checked === true) {
+    message.innerText = `
+      ${returnRandomIndex(data.affirmations)}
+    `
+  } else if (mantraSelection.checked === true) {
+    message.innerText = `
+      ${returnRandomIndex(data.mantras)}
+    `
+  }
+}
+
+function deselectMessage() {
+  if (!event.target.classList.contains('deselect-proof')) {
+    affirmationSelection.checked = false
+    mantraSelection.checked = false
+    message.classList.add('hidden')
+    yogaFigure.classList.remove('hidden')
+  }
+}
